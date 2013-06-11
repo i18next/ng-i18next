d0a6b79e36c72e0b8a412d98ae7f73b086323d34
Andre Meyering <info@andremeyering.de>
2013-06-11 16:59:12 +0200

    add examples
    add example files

---
+++ examples/example1/example1.js
@@ -0,0 +1,25 @@
+angular.module('MyApp', ['i18next']).config(function () {
+
+
+}).run(function ($rootScope) {
+
+	'use strict';
+
+	$rootScope.i18nextOptions = {
+		lng: 'de',
+		useCookie: false,
+		useLocalStorage: false,
+		fallbackLng: 'dev',
+		resGetPath: '../locales/__lng__/__ns__.json'
+	};
+
+});
+
+angular.module('MyApp').controller('MyCtrl', function ($rootScope, $scope) {
+
+	$scope.numbers =  ['one', 'two', 'three', 'four'];
+
+	$scope.hello = "content"
+	$scope.helloHTML = "<h3>content</h3>"
+
+});
