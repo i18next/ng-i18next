c04952f9e3e339a5dc698090e2490cb16e42b384
Ben Loveridge <benl@gradecam.com>
2014-07-16 14:03:09 -0600

    Look for defaultValue and defaultLoadingValue in the globalOptions object

--- src/provider.js
+++ src/provider.js
@@ -58,7 +58,8 @@ angular.module('jm.i18next').provider('$i18next', function () {
 
 			if (!t) {
 				translations[lng][key] = 'defaultValue' in options ? options.defaultValue :
-					'defaultLoadingValue' in options ? options.defaultLoadingValue : key;
+					'defaultValue' in globalOptions ? globalOptions.defaultValue :
+					'defaultLoadingValue' in globalOptions ? globalOptions.defaultLoadingValue : key;
 			} else if (!translations[lng][key] || hasOwnOptions) {
 				translations[lng][key] = t(key, options);
 			}
