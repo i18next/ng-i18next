ed2f31a6df84cbc5875c2a01b53468ab27a603b0
Andre Meyering <info@andremeyering.de>
2014-07-18 16:43:00 +0200

    add defaultValue example

---
+++ examples/directive/defaultValue.js
@@ -0,0 +1,22 @@
+angular.module('jm.i18next').config(function ($i18nextProvider) {
+
+	'use strict';
+
+	$i18nextProvider.options = {
+		useCookie: false,
+		useLocalStorage: false,
+		resGetPath: '../locales/__lng__/__ns__.json'
+	};
+
+});
+
+angular.module('MyApp', ['jm.i18next']).controller('MyDirectiveCtrl', function ($rootScope, $scope, $timeout, $i18next) {
+
+	'use strict';
+
+	$scope.changeLng = function (lng) {
+		$i18next.options.lng = lng;
+		console.log($i18next.debugMsg[$i18next.debugMsg.length - 1]);
+	};
+
+});
