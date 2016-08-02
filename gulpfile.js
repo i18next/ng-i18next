9dc44f364102d78f5e0e6c15d198713bbb09e448
anwalkers <andy.walkers@gmail.com>
2016-08-02 16:04:17 -0700

    let to var

--- gulpfile.js
+++ gulpfile.js
@@ -19,10 +19,10 @@ var bump = require('gulp-bump'),
 
 getToday = function () {
 
-	let today = new Date();
-	let dd = 	today.getDate();
-	let mm = 	today.getMonth() + 1; //January is 0!
-	let yyyy =  today.getFullYear();
+	var today = new Date();
+	var dd = 	today.getDate();
+	var mm = 	today.getMonth() + 1; //January is 0!
+	var yyyy =  today.getFullYear();
 
 	if (dd < 10) {
 		dd = '0' + dd;
